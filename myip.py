022871049d0678b372241ed18c7a874d6bca7f5b
Jeremy West <jeremy@visionswest.com.au>
2015-09-16 17:59:46 +1000

    Cleaned up code, added #TODO notes into main code.

--- myip.py
+++ myip.py
@@ -6,24 +6,25 @@ import time
 import subprocess
 import sched
 
+# Configure logging & output file for log
 logging.basicConfig(level=logging.INFO, 
                     filename='myip_log.txt', # log to this file
                     format='%(asctime)s %(message)s') # include timestamp
 
+# Configure scheduler to run function below
 s = sched.scheduler(time.time, time.sleep)
 
+# IP Address Logging Function (function required to ensure it can be run in a schedule)
 def log_ip_address(sc):
   print ("Logging IP Now...")
   for ifaceName in interfaces():
       addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
-      #print '%s: %s' % (ifaceName, ', '.join(addresses))
       logging.info('%s: %s' % (ifaceName, ', '.join(addresses)))
       sc.enter(60,1,log_ip_address,(sc,))
 
+# Need to add escape clause. 
+# Need to add log message when IP Address is lost.
+# Print IP Lost to terminal while program is running + log file. 
+
 s.enter(1,60,log_ip_address,(s,))
 s.run()
-
-
-
-#while True:
-  #log_ip_address()
